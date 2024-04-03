11a5a89fcdcfd0fbc2323ad87ed11dd8b8bcce80
liana <liana.tsaturyan.a@gmail.com>
2024-04-03 21:02:51 +0400

    admin / website / content

--- config/app.php
+++ config/app.php
@@ -59,6 +59,7 @@ return [
 
     'asset_url' => env('ASSET_URL'),
 
+    'frontend_url' => env('FRONTEND_URL', 'http://localhost'),
     /*
     |--------------------------------------------------------------------------
     | Application Timezone
