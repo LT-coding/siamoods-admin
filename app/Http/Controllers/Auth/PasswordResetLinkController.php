7418b8927190eeb2fa76f9940bfc85ef3faf45fd
liana <liana.tsaturyan.a@gmail.com>
2024-04-26 17:00:05 +0400

    auth password reset

--- app/Http/Controllers/Auth/PasswordResetLinkController.php
+++ app/Http/Controllers/Auth/PasswordResetLinkController.php
@@ -17,7 +17,7 @@ class PasswordResetLinkController extends Controller
      */
     public function create(): View
     {
-        return view('auth.forgot-password');
+        return view('auth.passwords.email');
     }
 
     /**
