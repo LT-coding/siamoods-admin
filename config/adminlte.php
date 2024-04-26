2a65a2cc588ae3013f24a1df173bf684787f9553
liana <liana.tsaturyan.a@gmail.com>
2024-04-26 17:13:38 +0400

    auth password reset

--- config/adminlte.php
+++ config/adminlte.php
@@ -257,7 +257,7 @@ return [
     'logout_url' => 'logout',
     'login_url' => 'login',
     'register_url' => false,
-    'password_reset_url' => 'forgot-password',
+    'password_reset_url' => 'reset-password',
     'password_email_url' => 'forgot-password',
     'profile_url' => 'admin/profile',
 
