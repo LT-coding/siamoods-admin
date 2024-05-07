95292b8c94cc1a782f8f4b85b039b794b2624d8b
liana <liana.tsaturyan.a@gmail.com>
2024-05-07 16:17:24 +0400

    Api / Social login

--- app/Http/Controllers/Social/SocialAuthController.php
+++ app/Http/Controllers/Social/SocialAuthController.php
@@ -2,50 +2,36 @@
 
 namespace App\Http\Controllers\Social;
 
+use App\Enums\RoleTypes;
 use App\Http\Controllers\Controller;
-use App\Providers\RouteServiceProvider;
-use Illuminate\Http\JsonResponse;
-use Laravel\Socialite\Facades\Socialite;
-use Symfony\Component\HttpFoundation\RedirectResponse;
+use App\Models\User;
+use Illuminate\Auth\Events\Registered;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
 
 class SocialAuthController extends Controller
 {
-    use UserAuthOrCreate;
-
-    // Facebook login
-    public function redirectToFacebook(): RedirectResponse|\Illuminate\Http\RedirectResponse
+    // Social login
+    public function store($userData, $provider): void
     {
-        return Socialite::driver('facebook')->redirect();
-    }
+        $user = User::query()->firstOrCreate(
+            ['email' => $userData['email']],
+            [
+                'name' => $userData['name'],
+                'lastname' => $userData['lastname'],
+                'password' => Hash::make(Str::random(8)),
+                'status' => 0,
+                'provider_id' => $provider,
+            ]
+        );
+        $user->markEmailAsVerified();
+        $user->assignRole(RoleTypes::account->name);
 
-    // Facebook callback
-    public function handleFacebookCallback(): JsonResponse
-    {
-        // Get user
-        $user = Socialite::driver('facebook')->user();
-        // Auth  User
-        $this->register_or_login($user, "facebook");
-        // Return profile page after login
-        return response()->json([
-            'status' => "success",
-        ]);
-    }
+        event(new Registered($user));
 
+//        TODO need to send email
 
-    // Google login
-    public function redirectToGoogle(): RedirectResponse|\Illuminate\Http\RedirectResponse
-    {
-        return Socialite::driver('google')->redirect();
-    }
-
-    // Google callback
-    public function handleGoogleCallback(): JsonResponse
-    {
-        $user = Socialite::driver('google')->stateless()->user();
-        $this->register_or_login($user, "google");
-        // Return profile page after login
-        return response()->json([
-            'status' => "success",
-        ]);
+        Auth::login($user);
     }
 }
--- routes/social-auth.php
+++ routes/social-auth.php
@@ -3,12 +3,4 @@
 use App\Http\Controllers\Social\SocialAuthController;
 use Illuminate\Support\Facades\Route;
 
-Route::controller(SocialAuthController::class)->group(function () {
-    // Google login
-    Route::any('login/google', 'redirectToGoogle')->name('login.google');
-    Route::get('login/google/callback', 'handleGoogleCallback');
-
-    // Facebook login
-    Route::any('login/facebook', 'redirectToFacebook')->name('login.facebook');
-    Route::get('login/facebook/callback', 'handleFacebookCallback');
-});
+Route::post('social-login', [SocialAuthController::class, 'store']);
