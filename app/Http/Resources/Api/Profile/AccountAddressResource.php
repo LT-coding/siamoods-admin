3dca4c94d3504004b66366993f9e303b72d15821
liana <liana.tsaturyan.a@gmail.com>
2024-05-08 15:18:22 +0400

    Api / Account

--- app/Http/Resources/Api/Profile/AccountAddressResource.php
+++ app/Http/Resources/Api/Profile/AccountAddressResource.php
@@ -15,14 +15,10 @@ class AccountAddressResource extends JsonResource
     public function toArray(Request $request): array
     {
         return [
-            'id' => $this->getKey(),
-            'user_id' => $this->user_id,
-            'type' => $this->type,
             'name' => $this->name,
             'lastname' => $this->lastname,
             'phone' => $this->phone,
             'address_1' => $this->address_1,
-            'address_2' => $this->address_2,
             'country' => $this->country,
             'city' => $this->city,
             'state' => $this->state,
